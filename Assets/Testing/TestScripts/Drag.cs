566591d54126c31255822d480ded1117cc9bf1b8
Cat <angelcah321@hotmail.com>
2023-02-27 16:40:19 +0000

    Wheel of Fortune and Puzzle

--- Assets/Testing/TestScripts/Drag.cs
+++ Assets/Testing/TestScripts/Drag.cs
@@ -1,19 +1,39 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Drag : MonoBehaviour
 {
     public Canvas canvas;
 
+    public bool AbleToDrag = true;
 
+    public string Position;
+
+    public Vector3 GoalPosition;
 
     public void DragHandler(BaseEventData data)
     {
-        PointerEventData pointerData = (PointerEventData)data;
+        if(AbleToDrag)
+        {
+            PointerEventData pointerData = (PointerEventData)data;
 
-        Vector2 position;
-        RectTransformUtility.ScreenPointToLocalPointInRectangle((RectTransform)canvas.transform,pointerData.position,canvas.worldCamera, out position);
+            Vector2 position;
 
-        transform.position = canvas.transform.TransformPoint(position);
+            RectTransformUtility.ScreenPointToLocalPointInRectangle((RectTransform)canvas.transform, pointerData.position, canvas.worldCamera, out position);
+
+            transform.position = canvas.transform.TransformPoint(position);
+
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    { 
+        if(other.name == Position)
+        {
+            //transform.position = canvas.transform.TransformPoint(GoalPosition);
+
+            AbleToDrag = false;
+        }
     }
 }
